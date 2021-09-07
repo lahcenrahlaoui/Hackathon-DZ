eeafb52e2fc88f9f1c4ac4c6a01a4c04eec7cddb
Borni Ahmed Rami <borniahmedrami@gmail.com>
2021-09-07 15:07:44 +0100

    [Init] add files

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app dark class='bgmain'>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.bgmain{
+  background: url("/backgrounds/bg0.png") no-repeat center center fixed !important;
+  -webkit-background-size: cover !important;
+  -moz-background-size: cover !important;
+  -o-background-size: cover !important;
+  background-size: cover !important;
+  height: 100% !important;
+}
+</style>
---
+++ plugins/countdown.js
@@ -0,0 +1,4 @@
+import vueAwesomeCountdown from 'vue-awesome-countdown'
+import Vue from 'vue';
+
+Vue.use(vueAwesomeCountdown, 'vac') // Component name, `countdown` and `vac` by default
