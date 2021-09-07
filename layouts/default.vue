a3cbe98ffb8595bc43be86364a696ccd2ed9b45e
Borni Ahmed Rami <borniahmedrami@gmail.com>
2021-09-07 16:16:27 +0100

    [Init] init dev branch

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,7 @@
 <template>
-  <v-app dark class='bgmain'>
-
+  <v-app dark>
     <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
+        <nuxt/>
     </v-main>
   </v-app>
 </template>
@@ -20,12 +17,5 @@ export default {
 </script>
 
 <style>
-.bgmain{
-  background: url("/backgrounds/bg0.png") no-repeat center center fixed !important;
-  -webkit-background-size: cover !important;
-  -moz-background-size: cover !important;
-  -o-background-size: cover !important;
-  background-size: cover !important;
-  height: 100% !important;
-}
+
 </style>
