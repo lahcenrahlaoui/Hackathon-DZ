34b113edfeefda3ddd3ed2871938d627e1938e0c
Borni Ahmed Rami <borniahmedrami@gmail.com>
2021-09-08 02:23:31 +0100

    [Add] missing indexLayout

---
+++ layouts/indexLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <nuxt/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'indexLayout'
+}
+</script>
+
+<style scoped>
+
+</style>
